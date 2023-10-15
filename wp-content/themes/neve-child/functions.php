bb0d50e07acc5f89cd4f8435b43b20122f174071
Nassime Kherbouche <nk7594@hotmail.com>
2023-10-15 21:39:50 +0200

    ajout du hook sur la page admin malgré les défis avec le header

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -23,7 +23,7 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
 function add_admin_link($items, $args) {
-    if (is_user_logged_in() && $args->theme_location == 'menu-1') {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
         $items .= '<li><a href="' . get_admin_url() .'" >Admin</a></li>';
 
     }

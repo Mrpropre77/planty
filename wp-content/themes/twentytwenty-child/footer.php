deb4144eaa77bc1c4c3da95ddd893da5989d6617
Nassime Kherbouche <nk7594@hotmail.com>
2023-10-25 12:59:45 +0200

    modif du custom du theme enfant et création des pages

---
+++ wp-content/themes/twentytwenty-child/footer.php
@@ -0,0 +1,8 @@
+			<footer id="site-footer" class="header-footer-group">
+			<a href="http://localhost:8888/planty/">Mentions légales</a>
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
